f3cb0fb8f1595eaa0a5045f1bf04827bd3ee1a11
Rod <snakepliskin30@gmail.com>
2023-11-08 12:36:26 +0800

    grouped tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './greet';
 
-test('Greet renders correctly', () => {
-  render(<Greet />);
-  const element = screen.getByText(/hello/i);
-  expect(element).toBeInTheDocument();
-});
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
+    const element = screen.getByText(/hello/i);
+    expect(element).toBeInTheDocument();
+  });
 
-test('Greet renders with a name', () => {
-  render(<Greet greetName='Rod' />);
-  const element = screen.getByText(/hello rod/i);
-  expect(element).toBeInTheDocument();
+  test('renders with a name', () => {
+    render(<Greet greetName='Rod' />);
+    const element = screen.getByText(/hello rod/i);
+    expect(element).toBeInTheDocument();
+  });
 });
